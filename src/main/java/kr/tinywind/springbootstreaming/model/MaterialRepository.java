a0f3c58972fb7d7f801024244dc932eaf4f9438d
Anthony Gaudino <anthonygaudino.45@gmail.com>
2018-03-12 15:04:07 +0100

    Removed all database and CSV stuff

--- src/main/java/kr/tinywind/springbootstreaming/model/MaterialRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.tinywind.springbootstreaming.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-}
